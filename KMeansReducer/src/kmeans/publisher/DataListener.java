faec953bcac62415241e19a2297d8fd2a9ab16f7
filareta <filaretayordanova@gmail.com>
2015-11-16 19:36:54 +0200

    Parse published clusters and reduce

--- KMeansReducer/src/kmeans/publisher/DataListener.java
+++ KMeansReducer/src/kmeans/publisher/DataListener.java
@@ -9,50 +9,49 @@ import cluster.Point4D;
 
 import com.pcbsys.nirvana.client.nConsumeEvent;
 import com.pcbsys.nirvana.client.nEventListener;
+import com.pcbsys.nirvana.client.nEventProperties;
 
 public class DataListener implements nEventListener {
+    private final int EVENT_LIMIT_COUNT = 1;
+    
     private ClusteringPublisher publisher;
     private Reducer reducer;
     private ReentrantLock lock;
+    private int receivedEvents;
 
     public DataListener() {
 	publisher = new ClusteringPublisher();
 	lock = new ReentrantLock();
 	reducer = new Reducer();
+	receivedEvents = 0;
     }
 
     @Override
     public void go(nConsumeEvent event) {
 	lock.lock();
-
-	Clustering one = new Clustering();
-	Clustering two = new Clustering();
-
-	Cluster cl1 = new Cluster(new Point4D(5.40, 2.3, 1.4, 12), 4, 34.2,
-		12.4, 10.5, 48);
-	Cluster cl2 = new Cluster(new Point4D(8.40, 4.3, 8.4, 6), 5, 39.2,
-		24.4, 21.5, 24);
-	Cluster cl3 = new Cluster(new Point4D(12.40, 32.3, -12.4, 8), 3, 39.2,
-		24.4, 21.5, 24);
-	Cluster cl4 = new Cluster(new Point4D(6.40, 3.3, 2.4, 12), 2, 39.2,
-		24.4, 21.5, 24);
-	Cluster cl5 = new Cluster(new Point4D(9.40, 5.3, 8.4, 11), 3, 39.2,
-		24.4, 21.5, 24);
-	Cluster cl6 = new Cluster(new Point4D(10.40, 4.3, 8.4, 6), 5, 39.2,
-		14.4, 21.5, 24);
-
-	one.addCluster(cl1);
-	one.addCluster(cl2);
-	one.addCluster(cl3);
-	two.addCluster(cl4);
-	two.addCluster(cl5);
-	two.addCluster(cl6);
-
-	reducer.addClustering(one);
-	reducer.addClustering(two);
-
+	
+	nEventProperties props = event.getProperties();
+	nEventProperties clusters[] = (nEventProperties[]) props.get("clusters");
+	Clustering clustering = new Clustering();
+	
+	for(int i = 0; i < clusters.length; i++) {
+	    double[] centerValues = clusters[i].getDoubleArray("center");
+	    double[] sums = clusters[i].getDoubleArray("sum");
+	    int totalCount = clusters[i].getInt("pointsCount");
+	    
+	    Point4D center = new Point4D(centerValues[0], centerValues[1], centerValues[2], (int) centerValues[3]);
+	    Cluster cluster = new Cluster(center, totalCount, sums[0], sums[1], sums[2], (int) sums[3]);
+	    clustering.addCluster(cluster);
+	}
+	
+	reducer.addClustering(clustering);
+	
+	receivedEvents++;
+	if (receivedEvents == EVENT_LIMIT_COUNT) {
+	    publisher.publish(reducer.getClustering());
+	}
+	
 	lock.unlock();
-	publisher.publish(reducer.getClustering());
     }
 
 }
