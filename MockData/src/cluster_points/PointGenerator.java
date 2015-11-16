9ba921fcd1444b7c7efbe57cfb6583b07e96a1f5
chivankich <chivankich@gmail.com>
2015-11-16 11:33:40 +0200

    Add basic share point implementation

---
+++ MockData/src/cluster_points/PointGenerator.java
@@ -0,0 +1,13 @@
+package cluster_points;
+
+import java.util.LinkedList;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+public class PointGenerator {
+	private PointGenerator() {};
+	
+	public LinkedList<Point4D> generatePointsAround() {
+		LinkedList<Point4D> l = new LinkedList<Point4D>();
+		return l;
+	}
+}
